9be9b65aa00d314c25f7051defd9d57aacac3a5c
Charles <charles@kwa.digital>
2021-01-08 00:26:33 +0100

    load class tform_actions

--- parameter_reseller_edit.php
+++ parameter_reseller_edit.php
@@ -4,7 +4,7 @@ $app = require_once __DIR__ . '/lib/init_module.php';
 $tform_def_file = "form/parameter_reseller.tform.php";
 
 // Loading classes
-$app->uses('tpl,tform,tools_sites');
+$app->uses('tpl,tform,tform_actions,tools_sites');
 
 require_once __DIR__ . '/lib/classes/parameter_actions.php';
 
